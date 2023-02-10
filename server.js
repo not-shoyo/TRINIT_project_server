45619359b063518bf9a4b5e95e265149678d0ec5
not-shoyo <73631606+not-shoyo@users.noreply.github.com>
2023-02-11 00:32:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const port = 5000;
 
 const app = express();
 
+const { exec } = require("child_process");
+const { stdout } = require("process");
+
+app.get("/getSizeFromUrl", (req, res) => {
+  exec(`curl -so /dev/null ${req.body.url} -w '%{size_download}`, (err, stdout, stderr) => {
+    if (err) {
+      console.log("Shell error:", stderr);
+      return stderr;
+    }
+    return stdout;
+  }).then((result) => res.json({ resultSize: result }));
+});
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
